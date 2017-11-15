4f884603aae4eecba8d891923c43708fc80e4d97
Caio César Dantas <caio_z7_mga@hotmail.com>
2017-11-15 10:03:11 -0200

    Atualizando...

---
+++ DetectFaces/app.js
@@ -0,0 +1,22 @@
+
+function ConverteStringToSQL(faceID, nome, gender, age,url, hair,accessories, glasses, facialHair){
+	var sqlFaceID = faceID;
+	var sqlNome = nome;
+	var sqlIdade = age;
+	var sqlGenero = gender;
+	var sqlCabelo = hair;
+	var sqlAcessorios = accessories;
+	var sqlOculos = glasses;
+
+}
+
+function ConverteSQLtoArr(string){
+
+	//Uma funcao em recursiva com a ConverteStringToSQL para converte o insert,
+	//para um array de insert
+}
+
+var arrSQL = []
+function InsereDados(arrSQL){
+
+}
